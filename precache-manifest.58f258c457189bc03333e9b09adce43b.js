117be972f4a355b4ce431e9000a87d4666b63a1d
C1300504 Nataniel Freire de Paiva Amorim <c1300504@bb.com.br>
2019-11-04 19:36:20 -0300

    aa

---
+++ precache-manifest.58f258c457189bc03333e9b09adce43b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25947ec2223def47876058c954c7e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36a9d55b12fd61ede740",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c4435e9b23c385bc758c",
+    "url": "/static/js/2.e129b7dc.chunk.js"
+  },
+  {
+    "revision": "36a9d55b12fd61ede740",
+    "url": "/static/js/main.79bc2e75.chunk.js"
+  },
+  {
+    "revision": "b12c9cedc2ab2a44146c",
+    "url": "/static/js/runtime-main.4a84bbe1.js"
+  }
+]);
